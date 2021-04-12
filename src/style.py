74dae1a0d46d2efecf69f1cc2c06725e6b53aced
Zenovore <agustinus.alexander17@gmail.com>
2021-04-12 20:32:45 +0700

    Add Logo.png and style.py

---
+++ src/style.py
@@ -0,0 +1,5 @@
+
+
+br = {'size':(7,1),'font':('Roboto',24),'button_color' : ("black","#AAC63D")}
+bl = {'size':(7,1),'font':('Roboto',24),'button_color' : ("black","#AAC63D")}
+title = {'font':('Roboto',24),'text_color':'black','pad':'200'}
