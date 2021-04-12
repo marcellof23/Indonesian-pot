dca065a8e41b37c5eebe060c99d9fe99e36636e5
marcellof23 <01faria.marcello@gmail.com>
2021-04-13 02:12:14 +0700

    add market ui util

--- src/market.py
+++ src/market.py
@@ -1,25 +1,30 @@
 import PySimpleGUI as sg
 
 
-def makeLayout():
-    return [[sg.Text('tes1', background_color="#ccffff")], [sg.Text('tes2', background_color="#ccffff")]]
+def makeTanamanLayout():
+    return [[sg.Button(button_text='Tes', s=(12, 7), button_color='#ffccff'), ], [sg.Text('tes2', background_color="#ccffff", size=(12, 7))]]
 
 
 def marketDisplay(windowWidth: int, windowHeight: int):
     layout1 = [[sg.Text('Indonesian - Pot', background_color="#ccffff"),
-                sg.Input(k='QUERY', enable_events=True), sg.Button('Search'),sg.Button('Store'), sg.Button('Forum'), sg.Button('Tips & Tricks'), sg.Button('Cart'), sg.Button('< Prev'), sg.Button('Next >')]]
+                sg.Input(k='QUERY', enable_events=True), sg.Button('Search'), sg.Button('Store'), sg.Button('Forum'), sg.Button('Tips & Tricks'), sg.Button('Cart'), sg.Button('< Prev'), sg.Button('Next >')]]
 
-    layout2 = [[sg.Text('List Tanaman', background_color='white')]]
-    layout2 += [[sg.Column(makeLayout(), key='-COL3-',
-                           background_color='#ffccff')]]
-    layout2 += [[sg.Column(makeLayout(), key='-COL4-',
-                           background_color='#ffccff')]]
+    layout2 = [
+        [sg.Text('List Tanaman', background_color='white', justification='center')]]
+
+    container_layout2 = [[]]
+    list_layout2 = []
+    for i in range(5):
+        list_layout2 += sg.Column(makeTanamanLayout(), key=str(i),
+                                  background_color='#ffccff', pad=(125, 0)),
+
+    container_layout2 = [list_layout2]
+    layout2 += container_layout2
+
     layout = [[sg.Column(layout1, key='-COL1-', background_color='#ccffff',
-                         size=(windowWidth, 200))], [sg.Column(layout2, key='-COL2-', background_color='white', size=(windowWidth, windowHeight-300))]]
-    
-    return sg.Window('Market', layout, finalize=True, size=(windowWidth, windowHeight), location=(-10, 0))
+                         size=(windowWidth, 200))], [sg.Column(layout2, key='-COL2-', background_color='white', size=(windowWidth, windowHeight-300), justification='center')]]
 
-
+    return sg.Window('Market', layout, margins=(0, 0), finalize=True, size=(windowWidth, windowHeight), location=(-10, 0), element_justification='c', resizable=True)
     # layout = [
     #           [sg.Listbox(values=[], enable_events=True,
     #                       size=(150, 20), k='SEARCHRESULT')],
