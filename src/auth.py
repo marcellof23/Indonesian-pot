84b8803af3678b16f535740b93a26196d4a18acc
Zenovore <agustinus.alexander17@gmail.com>
2021-04-12 20:46:41 +0700

    Insert Logo and add line break

--- src/auth.py
+++ src/auth.py
@@ -3,30 +3,37 @@ import PySimpleGUI as sg
 import utilities
 
 
+
 def loginDisplay(windowWidth : int, windowHeight : int):
-    layout = [[sg.Text('Login',**style.title)],
+    layout = [[sg.Image(key="-IMAGE-",size=(20,20))],
+              [sg.Text('\nLogin\n',**style.title)],
               [sg.Input(k='EMAIL', enable_events=True)],
               [sg.Input(k='PASSWORD', enable_events=True)],
+              [sg.Text('',**style.title)],
               [sg.Button('Login',**style.bl), sg.Button('Register',**style.br)],
               [sg.Text(size=(20,1), k='ERRORMSG')]]
     window = sg.Window('Login', layout, finalize=True, location=(-10, 0), resizable=True, element_justification='c',margins=(0,250))
     window.maximize()
+    utilities.insertImage(500,500,window)
     utilities.setPlaceholder(window["EMAIL"], placeholderText="e-mail address")
     utilities.setPlaceholder(window["PASSWORD"], placeholderText="password")
     return window
 
 def registerDisplay(windowWidth : int, windowHeight : int):
-    layout = [[sg.Text('Register',**style.title)],
+    layout = [[sg.Image(key="-IMAGE-",size=(20,20))],
+              [sg.Text('\nRegister\n',**style.title)],
               [sg.Input(k='NAME', enable_events=True)],
               [sg.Input(k='EMAIL', enable_events=True)],
               [sg.Input(k='PASSWORD', enable_events=True)],
               [sg.Input(k='REPEATEDPASSWORD', enable_events=True)],
               [sg.Input(k='PHONE', enable_events=True)],
               [sg.Input(k='ADDRESS', enable_events=True)],
+              [sg.Text('',**style.title)],
               [sg.Button('Login',**style.bl), sg.Button('Register',**style.br)],
               [sg.Text(size=(20,1),  k='ERRORMSG')]]
     window = sg.Window('Register', layout, finalize=True, location=(-10, 0), resizable=True, element_justification='c',margins=(0,250))
     window.maximize()
+    utilities.insertImage(500,500,window)
     utilities.setPlaceholder(window["NAME"], placeholderText="your name")
     utilities.setPlaceholder(window["EMAIL"], placeholderText="e-mail address")
     utilities.setPlaceholder(window["PASSWORD"], placeholderText="password")
