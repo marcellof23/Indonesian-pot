e34325123e7f37e2438dbd726b4e36fdd4d90115
Denilsen Axel <55840688+DenilsenAxel@users.noreply.github.com>
2021-04-12 22:10:28 +0800

    Fitur search product ditambahkan, sisa tampilannya mau dibagusin

--- src/market.py
+++ src/market.py
@@ -1,7 +1,13 @@
 import PySimpleGUI as sg
 
-def marketDisplay(windowWidth : int, windowHeight : int):
-    layout = [[sg.Text('Market')],
-               [sg.Button('< Prev'), sg.Button('Next >')]]
 
-    return sg.Window('Window 2', layout, finalize=True, size= (windowWidth,windowHeight), location=(-10, 0))
+def marketDisplay(windowWidth: int, windowHeight: int):
+    layout = [[sg.Text('Market')],
+              [sg.Input(k='QUERY', enable_events=True)],
+              [sg.Button('Search')],
+              [sg.Listbox(values=[], enable_events=True,
+                          size=(150, 20), k='SEARCHRESULT')],
+              [sg.Text(size=(20, 1), k='ERRORMSG')],
+              [sg.Button('< Prev'), sg.Button('Next >')]]
+
+    return sg.Window('Market', layout, finalize=True, size=(windowWidth, windowHeight), location=(-10, 0))
