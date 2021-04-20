b1fe8dcd8f598eca61b18094634ae0f2457d0425
marcellof23 <01faria.marcello@gmail.com>
2021-04-20 23:34:33 +0700

    nyesuain dokum

--- src/test_cart.py
+++ src/test_cart.py
@@ -8,28 +8,37 @@ class TestCart:
     
     def test_cart_one(self):
         user = loginAuthController("abc", "bambang")
-        cart = getCartProduct(user)
-        assert cart[0]['title'] == "eceng gondok"
-    def test_cart_two(self):
-        user = loginAuthController("abc", "bambang")
         status = addCartProduct(user["_id"], "60731e2b67fb9a7089e20f4b")
         assert status == "UPDATE"
-    def test_cart_three(self):
+    def test_cart_two(self):
         user = loginAuthController("abc", "bambang")
         status = reduceCartProduct(user["_id"], "60731e2b67fb9a7089e20f4b")
         assert status == "UPDATE"
-    def test_cart_four(self):
+    def test_cart_three(self):
         user = loginAuthController("abc", "bambang")
         status = addProductToCart(user["_id"], "60731e2b67fb9a7089e20f4b", 2)
         assert status == "UPDATE"
-    def test_cart_five(self):
+    def test_cart_four(self):
         user = loginAuthController("abc", "bambang")
         stockEnough = addOrderFromCart(user)
-        assert stockEnough == False
-    def test_cart_six(self):
+        assert stockEnough == True
+    def test_cart_five(self):
         user = loginAuthController("adm", "123")
         listProduct = []
         clearProductFromCart(user)
         listProduct = getCartProduct(user)
         checkEmpty = checkIsCartEmpty(listProduct)
         assert checkEmpty == True
+    def test_cart_six(self):
+        user = loginAuthController("abc", "bambang")
+        status = "FALSE"
+        listProduct = []
+        status2 = addProductToCart(user["_id"], "60755c29c8f8a9c0caa47f76", 2)
+        status3 = addProductToCart(user["_id"], "60755c55c8f8a9c0caa47f77", 2)
+        listProduct = getCartProduct(user)
+        for x in range(len(listProduct)):
+            for y in range(len(listProduct)):
+                if listProduct[x]['_id'] != listProduct[y]['_id'] and listProduct[x]['title'] == listProduct[y]['title'] and x != y:
+                    status = "TRUE"
+                    break
+        assert status == "TRUE"
