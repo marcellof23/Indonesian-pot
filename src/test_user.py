8a1fd137f77345805bb69ca967cba2ebaf70ab1a
Jesson Yo <Aphostrophy@gmail.com>
2021-04-20 21:33:26 +0700

    finished authController unit testing

--- src/test_user.py
+++ src/test_user.py
@@ -10,5 +10,12 @@ class TestUser:
     def test_login_two(self):
         user = loginAuthController("13519079@std.stei.itb.ac.id","noobmaster123")
         assert str(user["_id"]) == "6074302706c2945460aed695" and user["nama"] == "Jesson Yo"
-    # def test_register_one(self):
-    #     user = registerAuthController("Jesson Yo","13519079@std.stei.itb.ac.id")
+    def test_login_three(self):
+        user = loginAuthController("adm","1234")
+        assert user == None
+    def test_register_one(self):
+        response = registerAuthController("Jesson Yo","13519079@std.stei.itb.ac.id","kaoskakifiraun","kaoskakifiraun","081311111111","Beverly Hills A207")
+        assert response == "EMAILALREADYREGISTERED"
+    def test_register_two(self):
+        response = registerAuthController("Jesson Yo","13519079@std.stei.itb.ac.id","kaoskakifiraun","gajahmada","081311111111","Beverly Hills A207")
+        assert response == "PASSNOTMATCH"
