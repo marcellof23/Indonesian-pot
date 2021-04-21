b1ad5b681e8e5ee14b7e451cd0ec29be36427ae6
marcellof23 <01faria.marcello@gmail.com>
2021-04-21 14:05:22 +0700

    balikin test_cart

--- src/test_cart.py
+++ src/test_cart.py
@@ -9,27 +9,30 @@ class TestCart:
     def test_cart_one(self):
         user = loginAuthController("abc", "bambang")
         status = addCartProduct(user["_id"], "60731e2b67fb9a7089e20f4b")
+        status2 = reduceCartProduct(user["_id"], "60731e2b67fb9a7089e20f4b")
         assert status == "UPDATE"
+        
     def test_cart_two(self):
         user = loginAuthController("abc", "bambang")
         status = reduceCartProduct(user["_id"], "60731e2b67fb9a7089e20f4b")
+        status2 = addCartProduct(user["_id"], "60731e2b67fb9a7089e20f4b")
         assert status == "UPDATE"
+        
     def test_cart_three(self):
         user = loginAuthController("abc", "bambang")
         status = addProductToCart(user["_id"], "60731e2b67fb9a7089e20f4b", 2)
+        status2 = reduceCartProduct(user["_id"], "60731e2b67fb9a7089e20f4b")
+        status2 = reduceCartProduct(user["_id"], "60731e2b67fb9a7089e20f4b")
         assert status == "UPDATE"
+        
     def test_cart_four(self):
         user = loginAuthController("abc", "bambang")
+        status = addCartProduct(user["_id"], "60731e2b67fb9a7089e20f4b")
         stockEnough = addOrderFromCart(user)
-        assert stockEnough == True
+        status = reduceCartProduct(user["_id"], "60731e2b67fb9a7089e20f4b")
+        assert stockEnough == False
+        
     def test_cart_five(self):
-        user = loginAuthController("adm", "123")
-        listProduct = []
-        clearProductFromCart(user)
-        listProduct = getCartProduct(user)
-        checkEmpty = checkIsCartEmpty(listProduct)
-        assert checkEmpty == True
-    def test_cart_six(self):
         user = loginAuthController("abc", "bambang")
         status = "FALSE"
         listProduct = []
@@ -41,4 +44,9 @@ class TestCart:
                 if listProduct[x]['_id'] != listProduct[y]['_id'] and listProduct[x]['title'] == listProduct[y]['title'] and x != y:
                     status = "TRUE"
                     break
+        status2 = reduceCartProduct(user["_id"], "60755c29c8f8a9c0caa47f76")
+        status2 = reduceCartProduct(user["_id"], "60755c29c8f8a9c0caa47f76")
+        status2 = reduceCartProduct(user["_id"], "60755c55c8f8a9c0caa47f77")
+        status2 = reduceCartProduct(user["_id"], "60755c55c8f8a9c0caa47f77")        
         assert status == "TRUE"
+        
