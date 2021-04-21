903616d66ef7fdf0cf2031105038bbff404ad4c8
Zenovore <agustinus.alexander17@gmail.com>
2021-04-21 22:05:42 +0700

    Add readme+remove text from cart +add logo to Profile, add screenshoot image

--- src/profile.py
+++ src/profile.py
@@ -1,8 +1,11 @@
 import PySimpleGUI as sg
 import style
+import utilities
 
 def profileDisplay(windowWidth : int,windowHeight : int,user : dict):
-    layout = [[sg.Text('Indonesian - Pot'), sg.Button('Store'), sg.Button('Cart'), sg.Button('Logout')],
+    layout = [[sg.Image(key="-IMAGE-", size=(20, 20)), sg.Button('Store'), sg.Button('Cart'), sg.Button('Logout')],
               [sg.Text('Nama \t:  ',**style.title),sg.Text(user['nama'],**style.title)],
               [sg.Text('Email \t:  ',**style.title),sg.Text(user['email'],**style.title)]]
-    return sg.Window('Profile', layout, finalize=True, size= (windowWidth,windowHeight), location=(-10, 0))
+    window = sg.Window('Profile', layout, finalize=True, size= (windowWidth,windowHeight), location=(-10, 0))
+    utilities.insertImage(500, 500, window)
+    return window
